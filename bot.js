076d709a3d6bfffb415a6f31ca2c5ba98e37e3f7
Swoot86 <67533842+Swoot86@users.noreply.github.com>
2020-11-10 23:43:09 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,26 +14,5 @@ client.on('guildMemberAdd', member => {
 client.on('guildMemberRemove', member => {
    member.guild.channels.get('775638198616719363').send('<:Nadzor:460625006045429778><:Nadzor:460625006045429778><:Nadzor:460625006045429778><:Nadzor:460625006045429778><:Nadzor:460625006045429778> **' + member.user + '**, беги! Жалкий трус! <:Nadzor:460625006045429778><:Nadzor:460625006045429778><:Nadzor:460625006045429778><:Nadzor:460625006045429778><:Nadzor:460625006045429778>');
    // channel-id - Надо вводить ид канала куда будет писаться это сообщение
-});
-
-var prefix = '!'
-
- client.on('message', message => {
-   
-    if(message.author === client.user) return;
-
-   //Произвольные сообщения от имени Валерии
-	 if(message.content.startsWith(prefix + 'say')) {
-  if(message.author.id == '775787284070662164' //@Defo
-    const args = message.content.slice(prefix.length).split('say');
-	  message.delete()
-  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
-  .catch(console.error);
-     		message.channel.sendMessage(message.content.slice(prefix.length).split('say'));
-      	 }
-         else {
-           return;
-         }
-   };
  client.on
 client.login(process.env.BOT_TOKEN);
