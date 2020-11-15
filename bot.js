bdd790a5f90af7ee45dbb75582d7d892fd7abfc3
Swoot86 <67533842+Swoot86@users.noreply.github.com>
2020-11-15 18:16:17 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ var prefix = '!'
   //|| message.author.id == '435498881040777231' //@White Rabbit#4620
   //|| message.author.id !== '331129112598937600' //@cq-w9gfo#9491 
   //|| message.author.id !== '523946123606491151' //@ivan-baraban#2410
-  || message.author.id !== '253263899556708353' //@StereoType#8186
+  //|| message.author.id !== '253263899556708353' //@StereoType#8186
   || message.author.id !== '440835676586377227') { //@Pakicetus#5323  
     const args = message.content.slice(prefix.length).split('say');
 	  message.delete()
